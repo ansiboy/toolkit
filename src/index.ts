fa76207bfd20e1e5aef504df63f9c1bde7331257
mai.shu <ansiboy@163.com>
2020-03-02 15:43:40 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { guid } from "./guid";
 export { pathContact } from "./path";
 export { Errors } from "./errors";
-export { Less } from "./less";
--- src/ui.ts
+++
@@ -1,1 +0,0 @@
-export * from "maishu-ui-toolkit";
