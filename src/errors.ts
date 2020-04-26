ae485cce0d09f66ee63b7c57491f335f01048ba1
mai.shu <ansiboy@163.com>
2020-04-26 19:35:53 +0800

    update

--- src/errors.ts
+++ src/errors.ts
@@ -5,14 +5,14 @@ export class Errors {
         error.name = name;
         return error;
     }
-    routeDataFieldNull(fieldName: string): Error {
+    routeDataFieldNull<T>(fieldName: keyof T): Error {
         let msg = `The ${fieldName} field of route data cannt be null.`;
         let error = new Error(msg);
         let name: keyof Errors = "routeDataFieldNull";
         error.name = name;
         return error;
     }
-    argumentFieldNull(fieldName: string, argumentName: string): Error {
+    argumentFieldNull<T>(fieldName: keyof T, argumentName: string): Error {
         let msg = `The ${fieldName} field of ${argumentName} cannt be null.`;
         let error = new Error(msg);
         let name: keyof Errors = "argumentFieldNull";
@@ -25,10 +25,6 @@ export class Errors {
         let name: keyof Errors = "argumentTypeIncorrect";
         error.name = name;
         return error;
-    }
-    queryResultTypeError() {
-        let msg = 'Type of the query result is expected as Array or DataSourceSelectResult.';
-        return new Error(msg);
     }
 }
 
