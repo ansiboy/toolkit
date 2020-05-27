f9ff8199e574539c4052b095fe8d91d3c5bac004
mai.shu <ansiboy@163.com>
2020-05-27 21:11:27 +0800

    update

--- src/path.ts
+++ src/path.ts
@@ -11,8 +11,15 @@ export function pathContact(...paths: string[]) {
     }
 
     let str = paths.join("/");
-    // 将一个或多个的 / 变为一个 /，例如：/shop/test// 转换为 /shop/test/
-    str = str.replace(/\/+/g, '/');
+    // 将一个或多个的 / 或者 变为一个 /，例如：/shop/test// 转换为 /shop/test/
+    // 或者 D:\shop\test\  转换为 D:/shop/test/
+    str = str.replace(/(\/+|\\+)/g, '/');
+
+    //======================================================
+    // fixed 把 http:// https:// 变为 http:/ https:/ 的 BUG
+    str = str.replace("http:/", "http://");
+    str = str.replace("https:/", "https://");
+    //======================================================
 
     return str;
 }
