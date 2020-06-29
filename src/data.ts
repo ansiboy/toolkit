c90b64a4ee6d53df74398e55fbdfe3289d9741f2
mai.shu <ansiboy@163.com>
2020-06-29 15:23:40 +0800

    移除 ArrayDataSource

--- src/data.ts
+++ src/data.ts
@@ -216,18 +216,3 @@ export type DataSourceArguments<T> = {
     sort?: (items: T[]) => T[],
 };
 
-export class ArrayDataSource<T> extends DataSource<T> {
-    constructor(items: T[]) {
-        super({
-            async select(args) {
-                if (args.sortExpression) {
-
-                }
-                let dataItems = items.slice(args.startRowIndex, args.startRowIndex + args.maximumRows)
-                let result = { dataItems, totalRowCount: items.length }
-                return result
-            }
-        })
-    }
-}
-// }
